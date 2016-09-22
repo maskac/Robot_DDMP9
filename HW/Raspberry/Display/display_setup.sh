b3ab1174208b41ed6fcc35f8921796aed08d1538
Martin Sobotka <Sobotkama@gmail.com>
2016-09-22 18:43:22 +0200

    Create display set-up script
    This script makes the Adafruit 2.2" display work with Raspberry Pi

---
+++ HW/Raspberry/Display/display_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#use chmod +x setup_display.sh first, or this script might not execute
+
+#run this script as root, or it won't work
+if [ "$(id -u)" != "0" ]; then
+	echo "You are not root. Please run this script with "sudo" in front of it"
+	exit 1
+fi
+echo
+read -p "Warning! This script will reboot your Raspberry wen it's done. Do you want to continue? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo Please wait, this WILL take a while. A long while. May be up to 20 minutes.
+  echo Go get a coffee
+  curl -SLs https://apt.adafruit.com/add-pin | bash
+  echo 10%
+  apt-get -y install raspberrypi-bootloader
+  echo 50%
+  apt-get -y install adafruit-pitft-helper
+  echo 90%
+  adafruit-pitft-helper -t 22
+  echo 100%
+  echo done
+  echo rebooting now
+  reboot
+fi
