c335ff0d3e8231a72b136c612dc3dd26e29124f1
Martin Sobotka <sobotkama@sobotkama.eu>
2016-10-16 21:17:20 +0200

    Added  mysql server detection
    also added comments

--- HW/Raspberry/status/status_check.py
+++ HW/Raspberry/status/status_check.py
@@ -5,6 +5,8 @@ import os
 import glob
 import time
 import RPi.GPIO as GPIO
+import subprocess
+import string
 ledPin = 11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(ledPin, GPIO.OUT)
@@ -13,24 +15,31 @@ GPIO.setup(ledPin, GPIO.OUT)
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 #a list of directories in which the script shall look for the temperature sensors
-dirList = ['/sys/bus/w1/devices/28-xxxx', '/sys/bus/w1/devices/28-xxxx', '/sys/bus/w1/devices/28-xxxx', '/sys/bus/w1/devices/28-xxxx']  #every "xxxx" will have to be replaced
+dirList = ['/sys/bus/w1/devices/28-xxxx', '/sys/bus/w1/devices/28-xxxx', '/sys/bus/w1/devices/28-xxxx', '/sys/bus/w1/devices/28-xxxx']  #the "xxxx"s will have to be replaced by the used ds18b20s' serial numbers
 
 
 def checkTempSensors():
     for dir in dirList:
-        if not os.path.isdir(dir):
+        if not os.path.isdir(dir):  #if the directory doesn't exist
             return False
     return True
 
 
 while True:
-    ok=True
-    if checkTempSensors() == False:
-        ok = False
+    ok=True         #beginning of loop, nothing is broken now.
+    if checkTempSensors() == False:         #if temp sensors are broken
+        ok = False                          #everything is no longer ok
     
-    if ok == True:
-        GPIO.output(ledPin,GPIO.HIGH)
-    else:
-        GPIO.output(ledPin,GPIO.LOW)
+
+    #checking if mysql is running
+    tmp = os.popen("ps -Af").read()         #load a list of all loading processes
     
-    time.sleep(10)
+
+    if "msql" not in tmp[:]:                #if mysql is not in the list
+        ok = False                          #everything is no longer ok
+    
+    if ok == True:                          #if everything is still ok
+        GPIO.output(ledPin,GPIO.HIGH)       #turn a led on
+    else:                                   #else
+        GPIO.output(ledPin,GPIO.LOW)        #turn the let off
+    time.sleep(10)                          #sleep for 10 seconds before repeating
