b96f79538472a1f7ca3d56f4a2601ac1ebb338ce
Sobotkama <Sobotkama@gmail.com>
2016-11-02 21:46:53 +0100

    templog.py should now be working

--- HW/Raspberry/templog/templog.py
+++ HW/Raspberry/templog/templog.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
+#Oh hello there,stranger.Wellcome to this lovely little script
+#What does it do, you ask?
+#It gets the temperature from a ds18b20 sensor, along with a crc from the sensor, the current date and time and the current unix timestamp,
+#And it then stores this data to /tmp/teplota.log in CSV format like so:
+#"ROBESEK TS1",date,time,unixtimestamp,temperature,crc
+
+
 import os
 import glob
 import time
-
-temp = 0 
-crc = 0
 
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
-deviceFolders = [glob.glob(base_dir + '28-xxx'), glob.glob(base_dir + '28-xxxx'), glob.glob(base_dir + '28-xxxxx')]
+device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
 def read_temp_raw():
@@ -21,14 +25,6 @@ def read_temp_raw():
     f.close()
     return lines
 
-def logTempo():
-    lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
 
 def getVars():
     lines = read_temp_raw()
@@ -39,21 +35,21 @@ def getVars():
         
         equals_pos = lines[1].find('crc=')
     if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        crc = float(temp_string) / 1000.0
+        crc_string = lines[1][equals_pos+2:]
+        crc = float(crc_string)
         
-        unixtime = int(time.time())
-        date = 1
-        time = 2
-
+    unixtime = int(time.time())
+    date = time.strftime("%d.%m.%Y")
+    time = time.strftime("%H:%M:%S")
+    return "ROBESEK TS1",date,curtime,unixtime,temp,crc
 
 def logTemp():
-    getVars
-
-
-
-
-
+    logString = ','.join(map(str, getVars()))
+    print(var)
+    logfile = open("/tmp/teplota.log","a")
+    logfile.write(var)
+    logfile.write("\n")
+    logfile.close()
 
 while True:
     logTemp()
