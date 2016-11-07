29396971c23670b826e657a6dd86d68dd49b911c
Robert Jaworski <Robert Jaworski>
2016-11-07 12:38:06 +0100

    Udělány komentáře, smazal jsem interface - nefunguje:(

--- SW/RPi/Python/log_RSSI.py
+++ SW/RPi/Python/log_RSSI.py
@@ -7,7 +7,7 @@ import subprocess
 #Main method, returns RSSI info.
 #Hlavní metoda, vrací RSSI info.
 def get_RSSI():
-    proc = subprocess.Popen(["iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True)
+    proc = subprocess.Popen(["iwlist", interface, "scan"],stdout=subprocess.PIPE, universal_newlines=True) #Variable interface doesn't exist.
     out, err = proc.communicate()
     cell_line = match(out.split("\n")[0],"Cell ")
     line = cell_line[-27:]
@@ -15,7 +15,7 @@ def get_RSSI():
     return int(signal)
 
 def get_signal_level(cell):
-    return matching_line(cell,"Quality=").split("Signal level=")[1]
+    return matching_line(cell,"Quality=").split("Signal level=")[1] #Signal level is on the same line as Quality. Síla signálu je na stejném řádku jako kvalita.
 
 def matching_line(lines, keyword):
     """Returns the first matching line in a list of lines. See match()"""
