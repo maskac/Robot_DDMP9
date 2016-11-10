1e77ddb4b9591482f08b4f029b141f654594f31c
Tonda12 <tonda.kala@seznam.cz>
2016-11-10 18:34:29 +0100

    Přidání komentářů do GPS.py

--- SW/RPi/Python/GPS.py
+++ SW/RPi/Python/GPS.py
@@ -1,5 +1,5 @@
 def GPS():
-  ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=0)
-  s = ser.read(100)
-  souradnice = s.split(",")
+  ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=0) // definování portu
+  s = ser.read(1000) // přečtení věty
+  souradnice = s.split(",") // uložení věty do pole
   return souradnice
