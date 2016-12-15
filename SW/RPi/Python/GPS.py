562239d6be522b2b717544174ce17bc8bfd5c4cc
Tonda12 <tonda.kala@seznam.cz>
2016-12-15 18:21:23 +0100

    Přidání importu knihovny

--- SW/RPi/Python/GPS.py
+++ SW/RPi/Python/GPS.py
@@ -1,4 +1,5 @@
 def GPS():
+  import serial # importování knihovny serial
   ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=0) # definování portu
   s = ser.read(1000) # přečtení věty
   while (s.find($GPGGA, beg=0, end=len(s)) == -1):
