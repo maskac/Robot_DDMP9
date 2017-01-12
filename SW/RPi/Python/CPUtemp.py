9d6c970625c55c1a94f45b66aae48c43e3142575
robert-jaworski <robert.jaworski@seznam.cz>
2017-01-12 19:13:50 +0100

    Debug
    Testování na raspberry

--- SW/RPi/Python/CPUtemp.py
+++ SW/RPi/Python/CPUtemp.py
@@ -5,3 +5,6 @@ import os
 def getCPUtemperature():
     res = os.popen('vcgencmd measure_temp').readline()
     return(float(res.replace("temp=","").replace("'C\n","")))
+
+  if (__name__=="__main__"):
+  print(getCPUtemperature())
