79c4306df36e33f914f00b7bb9f5fc9264cc4c8c
robert-jaworski <robert.jaworski@seznam.cz>
2017-03-09 18:57:04 +0100

    Restrukturalizace
    Předělávání struktury aby se dalo naimplementovat mqtt

--- SW/RPi/Python/log.py
+++ SW/RPi/Python/log.py
@@ -5,24 +5,73 @@ import log_RSSI
 import teplotaDS
 from datetime import datetime
 
-# Soubor pro ukládání logování
 log_file = "log.txt"
 
-log_str = ""
+#Log file format: date,souradnice,nadmorska_vyska,pocet_sat_GPS, kvalita_sig_GPS,teplota_cpu,teplote_senzoru_DS,rssi,akcelerometr
 
-#Formát zápisu: (datum a čas),(GPS souřadnice),(nadmořská výška),(počet satelitů),(kvalita signálu GPS),(CPU teplota),(teplota DS),(DS crc),(RSSI),(akcelerometr x),(akcelerometr y),(akcelerometr z) 
-log_string += str(datetime.now()) + ","
-#gps_p = GPS.souradnice()
-#log_string += gps_p[0] + gps_p[1] + " " + gps_p[2] + gps_p[3] "," #Například: 50N 15E
-#log_string += GPS.nad_morska_vyska() + ","
-#log_string += GPS.pocet_satelitu() + ","
-#log_string += GPS.kvalita_signalu_GPS() + "," # 0 = není signál, 1 = připojeno k GPS, 2 = differenciálně připojeno k GPS
-log_string += str(CPUtemp.getCPUtemperature()) + ","
-temp = teplotaDS.getDCtemp()
-log_string += str(temp[0]) + "," + temp[1] + ","
-log_string += str(log_RSSI.get_RSSI()) + ","
-adxl = adxl345.ADXL345().getAxes()
-log_string += str(adxl["x"]) + "," + str(adxl["y"]) + "," + str(adxl["z"])
+def log_all():
+  log_string = ""
+  log_string += gps_sou() + ","
+  log_string += gps_nm() + ","
+  log_string += gps_ps() + ","
+  log_string += gps_kval() + ","
+  log_string += temp_cpu() + ","
+  
+  ds = temp_ds()
+  if ds[1] == "": #V pripade dobreho crc
+    log_string += ds[0] + ","
+  else:#Kdyz ne ulozi chybu
+    log_string += "DSError,"
+  log_string += rssi() + ","
+  accel = acc()
+  log_string += str(adxl["x"]) + "," + str(adxl["y"]) + "," + str(adxl["z"])
+  
+  log_string = date() + log_string + "\n" #Cas na konec kdyby to slo pomalu
+  
+  print(log_string)
+  
+  #Odkomentovat az bude otestovano
+  #writer = open(log_file,"a")
+  #writer.write(log_string)
+  #writer.close()
 
-with open(file,"a") as writer:
-  writer.write(log_string)
+def log_fast():
+  log_string = ""
+  accel = acc()
+  log_string += str(adxl["x"]) + "," + str(adxl["y"]) + "," + str(adxl["z"])
+
+def date():
+  return str(datetime.now()).replace(" ", ",").split(".")[0]
+
+def gps_sou():
+  gps_p = GPS.souradnice()
+  return gps_p[0] + gps_p[1] + "," + gps_p[2] + gps_p[3]
+
+def gps_nm():
+  return str(GPS.nad_morska_vyska())
+
+def gps_ps():
+  return str(GPS.pocet_satelitu())
+
+def gps_kval():
+  return str(GPS.kvalita_signalu_GPS())
+
+def temp_cpu():
+  return str(CPUtemp.getCPUtemperature())
+
+def temp_ds():
+  return teplotaDS.getDCtemp()
+#dc_t = str(temp[0]) + "," + temp[1]
+
+def rssi():
+  return str(log_RSSI.get_RSSI())
+
+def acc():
+  return adxl345.ADXL345().getAxes()
+
+#acc = str(adxl["x"]) + "," + str(adxl["y"]) + "," + str(adxl["z"])
+
+#return log_string
+
+if (__name__=="__main__"):
+  log_all()
