e960f0986a7d758a1d849819245e3c5cae4023dc
robert-jaworski <robert.jaworski@seznam.cz>
2017-03-12 20:05:30 +0100

    Upravení logger.py
    Timer() se zavolá před voláním funkce z log.py - kdyby se to protáhlo tak se nezdrží.

--- SW/RPi/Python/logger.py
+++ SW/RPi/Python/logger.py
@@ -3,13 +3,13 @@ import log
 
 # Kazdych 5 sekund zavola log_all z log.py
 def log_all():
-  log.log_all()
   Timer(5, log_all, ()).start()
+  log.log_all()
 
 # Kazdou sekundu zavola log_fast z log.py
 def log_fast():
-  log.log_fast()
   Timer(1, log_fast, ()).start()
+  log.log_fast()
 
 #Nastartuje cyklus
 log_all()
