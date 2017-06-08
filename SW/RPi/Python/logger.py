6b4f487debb81b45be68a6ff7cbfc357b64e0c6e
robert-jaworski <robert.jaworski@seznam.cz>
2017-06-08 18:55:27 +0200

    Přejmenování
    Přejmenování funkcí

--- SW/RPi/Python/logger.py
+++ SW/RPi/Python/logger.py
@@ -11,19 +11,19 @@ writer.write(str(os.getpid()))
 writer.close()
 
 # Kazdych 5 sekund zavola log_all z log.py
-def log_all():
+def all_loop():
   if (os.path.exists(shut_file)):
     #os.system("sudo rm " + shut_file)
     sys.exit()
-  Timer(5, log_all, ()).start()
+  Timer(5, all_loop, ()).start()
   log.log_all()
 
 # Kazdou sekundu zavola log_fast z log.py
-def log_fast():
+def fast_loop():
   if (not os.path.exists(shut_file)):
-    Timer(1, log_fast, ()).start()
+    Timer(1, fast_loop, ()).start()
   log.log_fast()
 
 #Nastartuje cyklus
-Timer(1, log_fast, ()).start()
-log_all()
+Timer(1, fast_loop, ()).start()
+all_loop()
