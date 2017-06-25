1f376d017c8cdbea17bfe43363c989f84101f88e
Sobotkama <sobotkama@gmail.com>
2017-06-25 13:24:43 +0200

    removed 8x8 matrix, autoformating
    Smaller arduinos do not have the pins required to have both 8x8 matrix and adafruit motor shield, using nonexistent pins causes bugs.

--- SW/ARDUINO/lowEnd/lowEnd.ino
+++ SW/ARDUINO/lowEnd/lowEnd.ino
@@ -1,10 +1,9 @@
 /*
- R.O.B.E.S.E.K.
- Arduino code
- Both serials set to 115200 bauds, Serial terminator is "\n"
- Web pages:
-   Maxim7219 for 8x8 matrix: https://datasheets.maximintegrated.com/en/ds/MAX7219-MAX7221.pdf
-   8x8 matrix program: http://playground.arduino.cc/LEDMatrix/Max7219
+  R.O.B.E.S.E.K.
+  Arduino code
+  Both serials set to 115200 bauds, Serial terminator is "\n"
+  Web pages:
+   https://learn.adafruit.com/adafruit-motor-shield/library-install Adafruit motor shield
 */
 
 #include <AFMotor.h>
@@ -20,15 +19,7 @@ AF_DCMotor mRearRight(4);
 #define sHorizontalPin 10
 #define sVerticalPin 9
 
-//8x8 matrix
-#define Max7219_pinCLK 24
-#define Max7219_pinCS 26
-#define Max7219_pinDIN 22
-#define Max7219_shutdown 0x01       // not in shutdown mode, for it 0x00
-#define Max7219_brightness 0x0f     // brightness, range: 0x00 to 0x0f
-#define Max7219_testmode 0x00       // not in display-test mode, for it 0x01
-
-//Serials
+//Serial
 
 #define SerialBaud 115200 // Set speed for Serial port
 
@@ -36,37 +27,30 @@ AF_DCMotor mRearRight(4);
 // ======================= DRIVER PART - do not change unless you know what you are doing
 
 /*
- *  There is difference between serialRASP - Serial and serialESP - SerialESP,
- *    the first ones are booleans for online/offline serial
- *    the second ones are defines for serial ports
- *    
- *  Response headers:
- *   starting with 0: 00 waiting for peripheries, 01 ready for anything, 02 communication error, 03 fatal error, 09 others - for the opposite side something like pass it on
- *   starting with 1 - motor or servo
- *   starting with 2 - 8x8 matrix
- */
+    Response headers:
+     starting with 0: 00 waiting for peripheries, 01 ready for anything, 02 communication error, 03 fatal error, 09 others - for the opposite side something like pass it on
+     starting with 1 - motor or servo
+     starting with 2 - 8x8 matrix
+*/
 
-int j=0, rcPwmRange[] = {0,255};                   // j - temporary int, rcPwmRange - pwm range for motor "[from],[to]", changeable via "m:range [from],[to]"
-int currentValues[] = {80,0,0};                    // current values of motor speed (%), vertical and horizontal servo angle
-unsigned int job=0, i=0, rcCurrentPwm=0;           // job - number of current task, i - temporary uint, rcCurrentPwm - current pwm value aplied on motors
-byte serialOutput=true;                           // whether sned output to Serial or not
+int j = 0, rcPwmRange[] = {0, 255};                // j - temporary int, rcPwmRange - pwm range for motor "[from],[to]", changeable via "m:range [from],[to]"
+int currentValues[] = {100, 0, 0};                  // current values of motor speed (%), vertical and horizontal servo angle
+unsigned int job = 0, i = 0, rcCurrentPwm = 0;     // job - number of current task, i - temporary uint, rcCurrentPwm - current pwm value aplied on motors
 void doJob(), SendToMax(byte Ma, byte Mb), MaxWrite(byte Me), SetMotorSpeed(), SetVerticalServo(), SetHorizontalServo();      // just for compiler
 String serialInput, jobData;                       // serialInput - incoming string/serial buffer, jobData - data for current task
-#define jobsLength 21                              // number of elements in jobs - once it will be calculated (maybe...)
-const String jobs[]={"m:stop","m:for","m:back","m:left","m:right","m:brake","m:speed","m:speed++","m:speed--","m:range","help","matrix","c:vert","c:hor","c:rst","c:vert++","c:vert--","c:hor++","c:hor--",""};
-// jobs - the last one is always empty
+#define jobsLength 20                              // number of elements in jobs - once it will be calculated (maybe...)
+const String jobs[] = {"m:stop", "m:for", "m:back", "m:left", "m:right", "m:brake", "m:speed", "m:speed++", "m:speed--", "m:range", "help", "matrix", "c:vert", "c:hor", "c:rst", "c:vert++", "c:vert--", "c:hor++", "c:hor--", ""}; // jobs - the last one is always empty
 Servo sHorizontal;
 Servo sVertical;
 
 // =======SETUP=======
 
-void setup(){
-  // initialize serials
+void setup() {
+  // initialize serial
   Serial.begin(SerialBaud);
-  
-  // just to be sure ESP and Raspberry are loaded, they should send on start send something, now disabled - missing handlers on ESP and RASP
+
   Serial.println(F("01 Ready"));
-  while(Serial.available()) Serial.read();
+  while (Serial.available()) Serial.read();
   Serial.println(F("09 Send \"help\" or I will die on your bad commands"));
 
   // motor and servo initialization
@@ -78,46 +62,34 @@ void setup(){
   sHorizontal.attach(sHorizontalPin);
   sVertical.attach(sVerticalPin);
 
-  // initialize 8x8 matrix
-  pinMode(Max7219_pinCLK, OUTPUT);
-  pinMode(Max7219_pinCS, OUTPUT);
-  pinMode(Max7219_pinDIN, OUTPUT);
-  delay(10);    // wait for chip
-  SendToMax(0x0b, 0x07);    // just pass some magic numbers - initialization sequence, follow datasheet
-  SendToMax(0x09, 0x00);
-  SendToMax(0x0c, Max7219_shutdown);
-  SendToMax(0x0f, Max7219_testmode);
-  SendToMax(0x0a, Max7219_brightness);
-  for(i=1; i<9; i++) SendToMax(i, 0);    // empty all registers = turn all LEDs off
+  //8x8 Matrix removed - pins not available on some arduinos
 }
 
 // =======LOOP=======
 
 void loop() {
   // READER: serial
-  if(Serial.available() > 0 ) {
+  if (Serial.available() > 0) {
     // prepare data
     serialInput = Serial.readStringUntil('\n');
-    serialInput.replace("\n","");
-
+    serialInput.replace("\n", "");
     // split data into job and jobData, based on "contains parameter" or not
     job = (serialInput.indexOf(" ") + 1);
-    if(job == 0) {
-      for(i=0; i<jobsLength; i++) {
-        if(jobs[i] == serialInput) break;
+    if (job == 0) {
+      for (i = 0; i < jobsLength; i++) {
+        if (jobs[i] == serialInput) break;
       }
     }
     else {
       jobData = serialInput.substring(job);
       job--;
-      for(i=0; i<jobsLength; i++) {
-        if(jobs[i] == serialInput.substring(0, job)) break;    // look into jobs for match
+      for (i = 0; i < jobsLength; i++) {
+        if (jobs[i] == serialInput.substring(0, job)) break;   // look into jobs for match
       }
     }
-    if(i == jobsLength) job = 128;    // if no job found
+    if (i == jobsLength) job = 128;   // if no job found
     else job = i;    // if job found
     doJob();    // finally do some jobbing (nice englando)
-    serialOutput = false;    // reset "send response to Serial" status
   }
   // READER: ultrasonic sensors __WIP__
 }
@@ -126,7 +98,7 @@ void loop() {
 
 // RIP easy commands (since 18.03.17 not in use, using simple incremental int): 0-9 (ASCII:48-57) reserved for control, letters (ASCII:65-90) are for addons, response only to RASP ("H" for displaying help)
 void doJob() {
-  switch(job) {
+  switch (job) {
     case 0:    // Stop
       mFrontLeft.run(RELEASE);
       mRearLeft.run(RELEASE);
@@ -171,8 +143,8 @@ void doJob() {
       break;
     case 6:    // Set speed, data format: [percent], range 0-100, it's translated using pwmRange to current speed
       j = jobData.toInt();
-      if(j > 100) j=100;
-      else if(j < 0) j=0;
+      if (j > 100) j = 100;
+      else if (j < 0) j = 0;
       currentValues[0] = j;
       SetMotorSpeed();
       break;
@@ -186,17 +158,17 @@ void doJob() {
       break;
     case 9:    // Set speed range, data format: [from],[to], define lowest and highest value of pwmRange in range 0-255
       i = jobData.indexOf(',');
-      if(i < 0) {
+      if (i < 0) {
         Serial.println(F("02:Motors: bad range format"));
         break;
       }
-      j = jobData.substring(0,i).toInt();
-      if(j > 255) j=255;
-      else if(j < 0) j=0;
+      j = jobData.substring(0, i).toInt();
+      if (j > 255) j = 255;
+      else if (j < 0) j = 0;
       rcPwmRange[0] = j;
-      j = jobData.substring(i+1).toInt();
-      if(j > 255) j=255;
-      else if(j < 0) j=0;
+      j = jobData.substring(i + 1).toInt();
+      if (j > 255) j = 255;
+      else if (j < 0) j = 0;
       rcPwmRange[1] = j;
       {
         Serial.print(F("17 "));
@@ -211,7 +183,6 @@ void doJob() {
       }
       break;
     case 10:    // Help
-      if(serialOutput == false) break;
       Serial.print(F("09:Help:\n"));
       Serial.print(F(" Motors: \"m: \" and \"stop\" -> stop, \"for\" -> forward, \"back\" -> backward, \"left\" -> leftward, \"right\" -> rightward\n  \"brake\" -> brake, \"speed []\" -> set speed percent (0-100), \"range [from],[to]\" -> set speed range (0-255)\n"));
       Serial.print(F(" 8x8 matrix: \"matrix [address][data]\n  addresses: 0 for display, 1 for brightness, 2 for shutdown, 3 for test mode\n  data if display: 8 numbers in range 0-255 separated with comma, eg.: 232,104,234,052,195,023,154,042\n  data if other:\n   brightness from 0 to 15 (from low to high)\n   shutdown - \"0\" -> off, \"1\" -> on\n   test mode - \"0\" -> off, \"1\" -> on\n"));
@@ -219,33 +190,19 @@ void doJob() {
       Serial.println(F(" Serial Output Settings: \"serout \" and \"?\" -> display values, \"rasp\" -> switch value for Raspberry, \"esp\" -> switch value for ESP"));
       break;
     case 11:    // 8x8 matrix
-      switch(0 + jobData.charAt(0)) {
-        case 49: // print data
-          for(i=0; i<8; i++) SendToMax((i+1), jobData.substring((4*i+1), (4*i+4)).toInt());
-          break;
-        case 50: // brightness
-          SendToMax(0x0a, jobData.substring(1).toInt());
-          break;
-        case 51: // on/off
-          SendToMax(0x0c, jobData.substring(1).toInt());
-          break;
-        case 52: // test mode
-          SendToMax(0x0f, jobData.substring(1).toInt());
-          break;
-      }
-      Serial.println(F("09:8x8 matrix: done."));
+      Serial.println(F("09:8x8 matrix: not available on this arduino."));
       break;
     case 12:    // Vertical servo
       j = jobData.toInt();
-      if(j > 180) j=180;
-      else if(j < 0) j=0;
+      if (j > 180) j = 180;
+      else if (j < 0) j = 0;
       currentValues[1] = j;
       SetVerticalServo();
       break;
     case 13:    // Horizontal servo
       j = jobData.toInt();
-      if(j > 180) j=180;
-      else if(j < 55) j=55;
+      if (j > 180) j = 180;
+      else if (j < 55) j = 55;
       currentValues[2] = j;
       SetHorizontalServo();
       break;
@@ -277,9 +234,9 @@ void doJob() {
 
 // =======CURRENT VALUES=======             motor speed and servos
 void SetMotorSpeed() {
-  if(currentValues[0] > 100) currentValues[0]=100;
-  else if(currentValues[0] < 0) currentValues[0]=0;
-  rcCurrentPwm = rcPwmRange[0] + (((rcPwmRange[1]-rcPwmRange[0]) * currentValues[0])/100);    // I hope that you like math hehe
+  if (currentValues[0] > 100) currentValues[0] = 100;
+  else if (currentValues[0] < 0) currentValues[0] = 0;
+  rcCurrentPwm = rcPwmRange[0] + (((rcPwmRange[1] - rcPwmRange[0]) * currentValues[0]) / 100); // I hope that you like math hehe
   mFrontLeft.setSpeed(rcCurrentPwm);
   mRearLeft.setSpeed(rcCurrentPwm);
   mFrontRight.setSpeed(rcCurrentPwm);
@@ -291,8 +248,8 @@ void SetMotorSpeed() {
   }
 }
 void SetHorizontalServo() {
-  if(currentValues[2] > 180) currentValues[2]=180;
-  else if(currentValues[2] < 0) currentValues[2]=0;
+  if (currentValues[2] > 180) currentValues[2] = 180;
+  else if (currentValues[2] < 0) currentValues[2] = 0;
   sHorizontal.write(currentValues[2]);
   {
     Serial.print(F("18 "));
@@ -301,8 +258,8 @@ void SetHorizontalServo() {
   }
 }
 void SetVerticalServo() {
-  if(currentValues[1] > 180) currentValues[1]=180;
-  else if(currentValues[1] < 55) currentValues[1]=55;
+  if (currentValues[1] > 180) currentValues[1] = 180;
+  else if (currentValues[1] < 55) currentValues[1] = 55;
   sVertical.write(currentValues[1]);
   {
     Serial.print(F("19 "));
@@ -311,25 +268,4 @@ void SetVerticalServo() {
   }
 }
 
-// =======8x8 MATRIX=======        (I like copypasting... so much... I even copypasted my life... saaaad) -> from web source
-
-int Mc;
-byte Md;
-void SendToMax(byte Ma, byte Mb) {
-  digitalWrite(Max7219_pinCS, LOW);
-  MaxWrite(Ma);
-  MaxWrite(Mb);
-  digitalWrite(Max7219_pinCS, HIGH);
-}
-
-void MaxWrite(byte Me) {
-  Mc = 8;
-  while(Mc > 0) {
-    Md = 0x01 << (Mc - 1);                          // get bitmask
-    digitalWrite(Max7219_pinCLK, LOW);              // tick
-    if(Me & Md) digitalWrite(Max7219_pinDIN, HIGH); // choose bit, if true send 1
-    else digitalWrite(Max7219_pinDIN, LOW);         // send 0
-    digitalWrite(Max7219_pinCLK, HIGH);             // tock
-    --Mc;                                           // move to lesser bit
-  }
-}
+// =======8x8 MATRIX=======        Removed
